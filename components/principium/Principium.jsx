ecabfec4fdc45ea864efe09e533837b8dc5da83f
Golldan <cristiano.henriqueferreirabp@gmail.com>
2022-02-01 21:20:52 -0300

    feat: Componentes respondem ao input da Home

--- components/principium/Principium.jsx
+++ components/principium/Principium.jsx
@@ -1,11 +1,15 @@
-import { Mundus } from "./PrincipiumStyles"
+import { Mundus, Logo, Dummy, Center, Universe, Bidu } from "./PrincipiumStyles"
 import React, { Component, useEffect, useState } from "react";
+import FadeIn from "react-fade-in/lib/FadeIn";
 
 
 export default function Principium(){
 
     const init = ""
-    const [nomen, setNomen] = useState("")
+    const [logo, setLogo] = useState("");
+    const [nomen, setNomen] = useState("");
+    const [me, setMe] = useState("");
+    const [universe, setUniverse] = useState("");
 
     var nome = '';
     function mudaNome(e){
@@ -20,16 +24,54 @@ export default function Principium(){
     } 
 
     React.useEffect(() => {
+        if (nomen.substring(0, 7) == "cognito")
+            setLogo(<FadeIn><Logo /></FadeIn>);
+
+        if (nomen.substring(8, 12) == "est")
+            setMe(<FadeIn><h1>Cristiano Henrique Ferreira</h1></FadeIn>)
+
+        if (nomen.substring(12, 18) == "chuchu")
+            setUniverse(<Universe><Bidu></Bidu></Universe>)
+
+        console.log(nomen.substring(9, 12));
+
+    }, [nomen])
+
+    React.useEffect(() => {
         document.addEventListener('keyup', mudaNome)
-    }, []);
+    }, [ ]);
 
 
 
     return(
         <>
             <Mundus>
-                {nomen}
+                <Dummy >
+                    {logo}
+                </Dummy>
+                <Dummy >
+                
+                </Dummy>
+                <Dummy >
+                
+                </Dummy>
+                <Center>
+                {me}
+                    <br />
+                <div>{nomen}</div>
+                </Center>
+                <Dummy >
+                
+                </Dummy>
+                <Dummy >
+                
+                </Dummy>
+                <Dummy >
+                
+                </Dummy>
+                {/* {nomen} */}
             </Mundus>
+            {universe}
         </>
     )
 }
--- components/principium/PrincipiumStyles.jsx
+++ components/principium/PrincipiumStyles.jsx
@@ -4,15 +4,68 @@ export const Mundus = styled.section`
     width: 100vw;
     height: 100vh;
     background-color: rgb(10,10, 10);
-    /* display: grid;
-    grid-template-columns: 2fr 2fr 1fr;
-    grid-template-rows: repeat(4, 1fr); */
+    display: grid;
+    grid-template-columns: 1fr 2fr 1fr;
+    grid-template-rows: 1fr 2fr 1fr;
+    /* grid-gap: 1rem;
+    padding: 1rem; */
+    font-size: 5rem;
+    color:white;
+    /* background-image: url('dummyMountain.jpg'); */
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+`;
+
+export const Dummy = styled.div`
+    width:100%;
+    height: 100%;
+    background-color: white;
+    /* border: 1px solid black; */
+    color: black;
     display: flex;
     align-items: center;
     justify-content: center;
-    grid-gap: 1rem;
-    padding: 1rem;
-    font-size: 5rem;
-    color:white;
+    /* grid-row: 1;
+    grid-column: 1; */
 `;
 
+export const Center = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: white;
+    grid-row: 2;
+    grid-column: 1 / 4;
+    color: black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
+
+export const Logo = styled.div`
+    width:100px;
+    height: 100px;
+    background-image: url('moth.png');
+    background-size:contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    /* background-color: #887a7a; */
+`;
+
+export const Universe = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: white;
+    color: black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Bidu = styled.div`
+    width: 100px;
+    height: 100px;
+    background-image: url('bidu.png');
+    background-size: cover;
+`;
